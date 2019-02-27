992ef0811f6951052702a7f6ffe32f8e55ac05d5
Anthony Chu <38588759+anthonychu00@users.noreply.github.com>
2019-02-27 16:17:12 -0500

    initial

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php include 'includes/connection.php';?>
+<?php include 'includes/header.php';?>
+<?php include 'includes/navbar.php';?>
+
+<br><br>
+<link rel="stylesheet" type="text/css" href="styles.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="demo.css" media="all" />
+    <!-- jQuery -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <!-- FlexSlider -->
+    <script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+    <script type="text/javascript" charset="utf-8">
+    var $ = jQuery.noConflict();
+    $(window).load(function() {
+    $('.flexslider').flexslider({
+          animation: "fade"
+    });
+	
+	$(function() {
+		$('.show_menu').click(function(){
+				$('.menu').fadeIn();
+				$('.show_menu').fadeOut();
+				$('.hide_menu').fadeIn();
+		});
+		$('.hide_menu').click(function(){
+				$('.menu').fadeOut();
+				$('.show_menu').fadeIn();
+				$('.hide_menu').fadeOut();
+		});
+	});
+  });
+</script>
+
+     <div class="flexsliderDiv">
+		<div class="flexslider">
+	      <ul class="slides">
+	    	<li>
+	    		<a href="#"><img src="images/slider/slide1.jpg" alt="" title=""/></a>
+	    		<div class="flex-caption">
+                     <div class="caption_title_line"><h2>The Memebase</h2><p>Inspired by know your meme.</p></div>
+                </div>
+	    	</li>
+	    	<li>
+	    		<a href="#"><img src="images/slider/slide2.jpg" alt="" title=""/></a>
+	    		<div class="flex-caption">
+                     <div class="caption_title_line"><h2>Upload Files</h2><p>Suports file formats like PDF, PPT, DOC etc..</p></div>
+                </div>
+	    	</li>
+	    	<li>
+	    		<a href="#"><img src="images/slider/slide4.jpg" alt="" title=""/></a>
+	    		<div class="flex-caption">
+                     <div class="caption_title_line"><h2>Sign Up!</h2><p>Features User Authentication </p></div>
+                </div>
+	    	</li>
+	    </ul>
+	  </div>
+   </div>
+    </div>  
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'includes/footer.php';?>
+
+        
